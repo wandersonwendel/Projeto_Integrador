dbfea87a6bf2af04171dc4cfe2460c69bc6c91fe
wandersonwendel <wandsolopes@alu.ufc.br>
2023-10-06 14:43:22 -0300

    Update

--- server.py
+++ server.py
@@ -48,7 +48,7 @@ def cadastrar_veiculo(placa, crlv, fotoCNH, corVeiculo, modeloVeiculo, anoVeicul
         conn = psycopg2.connect(database="itaxi", user="postgres", password="1234", host="localhost", port="5432")
         cursor = conn.cursor()
 
-         # Verificar se o veículo já está cadastrado, com aquele cnh
+         # Verificar se o veículo já está cadastrado, com aquela placa
         cursor.execute("SELECT * FROM veiculos WHERE placa=%s", (placa,))
         veiculo = cursor.fetchone()
 
@@ -74,17 +74,63 @@ def cadastrar_veiculo(placa, crlv, fotoCNH, corVeiculo, modeloVeiculo, anoVeicul
     except Exception as e:
         print(f"Erro inesperado ao cadastrar o veículo: {e}")
 
+def solicitar_corrida(usuario, origem, destino):
+    try:
+        method_frame, header_frame, body = channel.basic_get(queue='fila_solicitar_corrida')
+        if not origem.strip() or not destino.strip():
+            raise ValueError("Origem e Destino não definidos! Por favor, inserir os campos.")
+    except Exception as e:
+        print(f"Erro inesperado ao solicitar corrida: {e}")
+
+def iniciar_corrida(motorista):
+    try:
+        # Simular um motorista aceitando a corrida
+        method_frame, header_frame, body = channel.basic_get(queue='iniciar_corrida')
+        if body:
+            print(f'Motorista {motorista} aceitou a corrida: {body}')
+        else:
+            print('Nenhuma corrida disponível.')
+    except Exception as e:
+        print(f"Erro ao iniciar a corrida: {e}")
+    finally:
+        # Fechar a conexão com o RabbitMQ
+        connection.close()
+   
+    
 def callback(ch, method, properties, body):
     mensagem = body.decode('utf-8')
+    
     nome, email, telefone, senha, endereco, sexo = mensagem.split(';')
-    placa, crlv, fotoCNH, corVeiculo, modeloVeiculo, anoVeiculo, renavam, numeroChassi = mensagem.split(';')
     senha = bcrypt.hashpw(senha.encode('utf-8'), bcrypt.gensalt())
+
+    placa, crlv, fotoCNH, corVeiculo, modeloVeiculo, anoVeiculo, renavam, numeroChassi = mensagem.split(';')
+    
+    usuario, origem, destino = mensagem.split(';')
+
+    motorista = mensagem.split(';')
+
     try:
         cadastrar_usuario(nome, email, telefone, senha, endereco, sexo)
         cadastrar_veiculo(placa, crlv, fotoCNH, corVeiculo, modeloVeiculo, anoVeiculo, renavam, numeroChassi)
+        solicitar_corrida(usuario, origem, destino)
+        iniciar_corrida(motorista)
     except Exception as e:
         print(f"Erro ao processar mensagem: {e}")
 
+def callback_iniciar_corrida(ch, method, properties, body):
+    #TODO extrair dados do body
+    mensagem = body.decode('utf-8')
+    iniciar_corrida(mensagem)
+    
+    return "Corrida iniciada"
+
+
+def callback_solicitar_corrida(ch, method, properties, body):
+    mensagem = body.decode('utf-8')
+    print(mensagem)
+    dados = mensagem.split(";")
+    solicitar_corrida(dados[0], dados[1], dados[2])
+    return "Corrida solicitada com sucesso"
 # Conectar ao RabbitMQ
 try:
     connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost', port=5672))
@@ -93,10 +139,14 @@ try:
     # Definir as filas a serem consumidas
     channel.queue_declare(queue='fila_cadastro')
     channel.queue_declare(queue='fila_cadastro_veiculo')
+    channel.queue_declare(queue='fila_solicitar_corrida')
+    channel.queue_declare(queue='fila_iniciar_corrida')
 
     # Configurar o callback para receber as mensagens
     channel.basic_consume(queue='fila_cadastro', on_message_callback=callback, auto_ack=True)
     channel.basic_consume(queue='fila_cadastro_veiculo', on_message_callback=callback, auto_ack=True)
+    channel.basic_consume(queue='fila_solicitar_corrida', on_message_callback=callback_solicitar_corrida, auto_ack=True)
+    channel.basic_consume(queue='fila_iniciar_corrida', on_message_callback=callback_iniciar_corrida, auto_ack=True)
 
     print('Aguardando mensagens...')
     channel.start_consuming()
--- solicitar_corrida.py
+++ solicitar_corrida.py
@@ -1,42 +1,34 @@
 import pika
 
-try:
-    # Configuração da conexão com o RabbitMQ
-    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+def enviar_mensagem(usuario, origem, destino):
+    # Enviar a solicitação de corrida para a fila
+    message = f'{usuario};{origem};{destino}'
+
+    # Conectar ao RabbitMQ
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost', port=5672))
     channel = connection.channel()
 
-    # Declarar uma fila para as solicitações de corrida
-    channel.queue_declare(queue='solicitacoes_corrida')
+    # Publicar a mensagem na fila de corrida
+    channel.basic_publish(exchange='', routing_key='fila_solicitar_corrida', body=message)
+    print(f'Mensagem enviada: {message}')
 
-    def solicitar_corrida(usuario, origem, destino):
-        try:
-            # Enviar a solicitação de corrida para a fila
-            message = f'Solicitação de corrida: {usuario}, de {origem} para {destino}'
-            channel.basic_publish(exchange='', routing_key='solicitacoes_corrida', body=message)
-            print(f'Solicitação de corrida enviada: {message}')
-        except Exception as e:
-            print(f"Erro ao enviar a solicitação de corrida: {e}")
+    connection.close()  
 
-    def iniciar_corrida(motorista):
-        try:
-            # Simular um motorista aceitando a corrida
-            method_frame, header_frame, body = channel.basic_get(queue='solicitacoes_corrida')
-            if body:
-                print(f'Motorista {motorista} aceitou a corrida: {body}')
-            else:
-                print('Nenhuma corrida disponível.')
-        except Exception as e:
-            print(f"Erro ao iniciar a corrida: {e}")
-        finally:
-            # Fechar a conexão com o RabbitMQ
-            connection.close()
 
-    # Exemplo de uso
-    solicitar_corrida('João', 'Aeroporto', 'Hotel')
-    solicitar_corrida('Maria', 'Estação de trem', 'Restaurante')
+def enviar_mensagem_iniciar_corrida(motorista):
+    channel.basic_publish(exchange='', routing_key='iniciar_corrida', body=message)
+    message = f'Motorista {motorista} aceitou a corrida'
 
+    # Conectar ao RabbitMQ
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost', port=5672))
+    channel = connection.channel()
+
+    print(f'Mensagem enviada: {message}')
+
+    connection.close()
     # Simular um motorista aceitando a corrida
-    iniciar_corrida('Motorista1')
+    
 
-except Exception as e:
-    print(f"Erro ao configurar a conexão com o RabbitMQ: {e}")
+enviar_mensagem('João', 'Aeroporto', 'Hotel')
+enviar_mensagem('Maria', 'Estação de trem', 'Restaurante')
+# enviar_mensagem_iniciar_corrida('Motorista1')
