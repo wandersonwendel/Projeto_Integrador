4f31049fc643ec4d58dcaa5ca1fef99639e6abe0
wandersonwendel <wandsolopes@alu.ufc.br>
2023-10-06 19:04:53 -0300

    Atualizando o código de  "solicitar_corrida.py"

--- solicitar_corrida.py
+++ solicitar_corrida.py
@@ -1,20 +1,5 @@
 import pika
 
-def enviar_mensagem(usuario, origem, destino):
-    # Enviar a solicitação de corrida para a fila
-    message = f'{usuario};{origem};{destino}'
-
-    # Conectar ao RabbitMQ
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost', port=5672))
-    channel = connection.channel()
-
-    # Publicar a mensagem na fila de corrida
-    channel.basic_publish(exchange='', routing_key='fila_solicitar_corrida', body=message)
-    print(f'Mensagem enviada: {message}')
-
-    connection.close()  
-
-
 def enviar_mensagem_iniciar_corrida(motorista):
     channel.basic_publish(exchange='', routing_key='iniciar_corrida', body=message)
     message = f'Motorista {motorista} aceitou a corrida'
@@ -32,3 +17,20 @@ def enviar_mensagem_iniciar_corrida(motorista):
 enviar_mensagem('João', 'Aeroporto', 'Hotel')
 enviar_mensagem('Maria', 'Estação de trem', 'Restaurante')
 # enviar_mensagem_iniciar_corrida('Motorista1')
+
+def solicitar_corrida(origem, destino, passageiro):
+    mensagem = f"{origem};{destino};{passageiro}"
+
+    # Conectar ao RabbitMQ
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost', port=5672))
+    channel = connection.channel()
+
+    # Publicar a mensagem na fila de solicitações de corrida
+    channel.basic_publish(exchange='', routing_key='fila_solicitacoes_corrida', body=mensagem)
+
+    print(f'Solicitação de corrida enviada: {mensagem}')
+
+    connection.close()
+
+# Exemplo de uso
+solicitar_corrida('Origem A', 'Destino B', 'João')
