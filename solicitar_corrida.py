a362d1cedf3db092ca2c2f47764476f6d08f089d
wandersonwendel <wandsolopes@alu.ufc.br>
2023-10-06 19:13:31 -0300

    Atualizando o código de  "solicitar_corrida.py"

--- solicitar_corrida.py
+++ solicitar_corrida.py
@@ -1,23 +1,5 @@
 import pika
 
-def enviar_mensagem_iniciar_corrida(motorista):
-    channel.basic_publish(exchange='', routing_key='iniciar_corrida', body=message)
-    message = f'Motorista {motorista} aceitou a corrida'
-
-    # Conectar ao RabbitMQ
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost', port=5672))
-    channel = connection.channel()
-
-    print(f'Mensagem enviada: {message}')
-
-    connection.close()
-    # Simular um motorista aceitando a corrida
-    
-
-enviar_mensagem('João', 'Aeroporto', 'Hotel')
-enviar_mensagem('Maria', 'Estação de trem', 'Restaurante')
-# enviar_mensagem_iniciar_corrida('Motorista1')
-
 def solicitar_corrida(origem, destino, passageiro):
     mensagem = f"{origem};{destino};{passageiro}"
 
@@ -34,3 +16,17 @@ def solicitar_corrida(origem, destino, passageiro):
 
 # Exemplo de uso
 solicitar_corrida('Origem A', 'Destino B', 'João')
+
+
+"""def enviar_mensagem_iniciar_corrida(motorista):
+    channel.basic_publish(exchange='', routing_key='iniciar_corrida', body=message)
+    message = f'Motorista {motorista} aceitou a corrida'
+
+    # Conectar ao RabbitMQ
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost', port=5672))
+    channel = connection.channel()
+
+    print(f'Mensagem enviada: {message}')
+
+    connection.close()
+    # Simular um motorista aceitando a corrida"""
